e216bf488577edc166af64a217510adde28810ba
tony <chcjswo@gmail.com>
2019-05-22 22:56:51 +0900

    문자열 연습

---
+++ stock/stock_1_1.py
@@ -0,0 +1,24 @@
+daum = 89000
+naver = 751000
+print(daum + naver)
+
+a = daum * 0.95
+b = naver * 0.9
+print(a)
+print(b)
+
+
+c = (84 - 32) / 1.8
+print(c)
+
+
+naver2 = 1000000 * 0.7 * 0.7 * 0.7
+print(naver2)
+
+d = "hi world"
+e = d[1:2] + d[0:1] + " " + d[3:]
+print(e)
+
+f = "abcdef"
+g = f[1:] + f[0:1]
+print(g)
